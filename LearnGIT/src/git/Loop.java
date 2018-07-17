b2adf767850a2c9d338c8ea313ce8b53c75c558f
Sam <samyuk93@gmail.com>
2018-07-17 17:32:36 +0530

    Commit the program file

---
+++ LearnGIT/src/git/Loop.java
@@ -0,0 +1,14 @@
+package git;
+
+public class Loop {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+for(int i=0;i<5;i++)
+{
+	System.out.println("amGROOT");
+}
+System.out.println("Ha ha ha ha ha:-D :-D :-D");
+	}
+
+}
